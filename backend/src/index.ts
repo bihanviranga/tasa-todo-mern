59ae2933cb2ee2ca041cdf83efa0fe8b46706a83
Bihan Viranga <bihanviranga@gmail.com>
2022-08-23 14:17:12 +0530

    Add empty Express project

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app: express.Application = express();
+
+// TODO move to env file with dotenv
+const port = 8080;
+
+app.get('/', (_req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
